b84e671f0f76081669c5f096470054eb2abdc16a
Pravin Bashyal <pravin.bashyal@gmail.com>
2019-11-13 01:47:32 +0100

    add examples

---
+++ example/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import { Button } from 'skeleton-react-kit'
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Button>Hello</Button>
+    </div>
+  )
+}
+
+export default App
